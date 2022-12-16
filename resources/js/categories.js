06e2ed485b3ce80407db66e243d2a50888b5ea6a
Gio <ggelashvili@gmail.com>
2022-12-15 21:51:51 -0500

    WIP

--- resources/js/categories.js
+++ resources/js/categories.js
@@ -1,10 +1,31 @@
+import { Modal } from "bootstrap"
+
 window.addEventListener('DOMContentLoaded', function () {
+    const editCategoryModal = new Modal(document.getElementById('editCategoryModal'))
+
     document.querySelectorAll('.edit-category-btn').forEach(button => {
         button.addEventListener('click', function (event) {
             const categoryId = event.currentTarget.getAttribute('data-id')
 
-            // TODO
-            console.log(categoryId)
+            // TODO: Fetch category info from controller & pass it to this function
+            openEditCategoryModal(editCategoryModal, {id: categoryId, name: ''})
         })
     })
+
+    document.querySelector('.save-category-btn').addEventListener('click', function (event) {
+        const categoryId = event.currentTarget.getAttribute('data-id')
+
+        // TODO: Post update to the category
+        console.log(categoryId)
+    })
 })
+
+function openEditCategoryModal(modal, {id, name}) {
+    const nameInput = modal._element.querySelector('input[name="name"]')
+
+    nameInput.value = name
+
+    modal._element.querySelector('.save-category-btn').setAttribute('data-id', id)
+
+    modal.show()
+}
