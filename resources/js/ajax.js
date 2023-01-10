a0b480dfc96266ba92a4c57c99ef8bb42b559c1d
Gio <ggelashvili@gmail.com>
2023-01-09 22:43:29 -0500

    WIP

--- resources/js/ajax.js
+++ resources/js/ajax.js
@@ -1,4 +1,4 @@
-const ajax = (url, method = 'get', data = {}) => {
+const ajax = (url, method = 'get', data = {}, domElement = null) => {
     method = method.toLowerCase()
 
     let options = {
@@ -12,16 +12,64 @@ const ajax = (url, method = 'get', data = {}) => {
     const csrfMethods = new Set(['post', 'put', 'delete', 'patch'])
 
     if (csrfMethods.has(method)) {
+        if (method !== 'post') {
+            options.method = 'post'
+
+            data = {...data, _METHOD: method.toUpperCase()}
+        }
+
         options.body = JSON.stringify({...data, ...getCsrfFields()})
     } else if (method === 'get') {
         url += '?' + (new URLSearchParams(data)).toString();
     }
 
-    return fetch(url, options).then(response => response.json())
+    return fetch(url, options).then(response => {
+        if (domElement) {
+            clearValidationErrors(domElement)
+        }
+
+        if (! response.ok) {
+            if (response.status === 422) {
+                response.json().then(errors => {
+                    handleValidationErrors(errors, domElement)
+                })
+            }
+        }
+
+        return response
+    })
 }
 
 const get  = (url, data) => ajax(url, 'get', data)
-const post = (url, data) => ajax(url, 'post', data)
+const post = (url, data, domElement) => ajax(url, 'post', data, domElement)
+const del = (url, data) => ajax(url, 'delete', data)
+
+function handleValidationErrors(errors, domElement) {
+    for (const name in errors) {
+        const element = domElement.querySelector(`input[name="${ name }"]`)
+
+        element.classList.add('is-invalid')
+
+        for (const error of errors[name]) {
+            const errorDiv = document.createElement('div')
+
+            errorDiv.classList.add('invalid-feedback')
+            errorDiv.textContent = error
+
+            element.parentNode.append(errorDiv)
+        }
+    }
+}
+
+function clearValidationErrors(domElement) {
+    domElement.querySelectorAll('.is-invalid').forEach(function(element) {
+        element.classList.remove('is-invalid')
+
+        element.parentNode.querySelectorAll('.invalid-feedback').forEach(function(e) {
+            e.remove()
+        })
+    })
+}
 
 function getCsrfFields() {
     const csrfNameField  = document.querySelector('#csrfName')
@@ -40,5 +88,6 @@ function getCsrfFields() {
 export {
     ajax,
     get,
-    post
+    post,
+    del
 }
--- resources/js/categories.js
+++ resources/js/categories.js
@@ -1,5 +1,5 @@
 import { Modal }     from "bootstrap"
-import { get, post } from "./ajax"
+import { get, post, del } from "./ajax"
 
 window.addEventListener('DOMContentLoaded', function () {
     const editCategoryModal = new Modal(document.getElementById('editCategoryModal'))
@@ -9,6 +9,7 @@ window.addEventListener('DOMContentLoaded', function () {
             const categoryId = event.currentTarget.getAttribute('data-id')
 
             get(`/categories/${ categoryId }`)
+                .then(response => response.json())
                 .then(response => openEditCategoryModal(editCategoryModal, response))
         })
     })
@@ -18,9 +19,19 @@ window.addEventListener('DOMContentLoaded', function () {
 
         post(`/categories/${ categoryId }`, {
             name: editCategoryModal._element.querySelector('input[name="name"]').value
-        }).then(response => {
-            console.log(response)
+        }, editCategoryModal._element).then(response => {
+            if (response.ok) {
+                editCategoryModal.hide()
+            }
         })
+    })
+
+    document.querySelector('.delete-category-btn').addEventListener('click', function (event) {
+        const categoryId = event.currentTarget.getAttribute('data-id')
+
+        if (confirm('Are you sure you want to delete this category?')) {
+            del(`/categories/${ categoryId }`)
+        }
     })
 })
 
