04bf35ce8bfe28c090f0cdcb9e654c1a3f277f8b
Gio <ggelashvili@gmail.com>
2023-02-07 19:55:36 -0500

    WIP

--- app/Controllers/ReceiptController.php
+++ app/Controllers/ReceiptController.php
@@ -4,7 +4,6 @@ declare(strict_types = 1);
 
 namespace App\Controllers;
 
-use App\Services\ReceiptService;
 use Psr\Http\Message\ServerRequestInterface as Request;
 use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\UploadedFileInterface;
